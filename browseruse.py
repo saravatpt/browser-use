cb0a9d1ba52bdd23f7afba8bd7ff78bd65593b94
Ramya chandrasekaran <Ramyachandrasekaran@conduent.com>
2025-05-01 18:23:34 +0530

    refactor: remove unused test file and clean up API key handling

--- browseruse.py
+++ browseruse.py
@@ -4,25 +4,36 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import SecretStr
-#from google.colab import userdata
 from browser_use import Agent
 
 load_dotenv()
-#os.environ['GOOGLE_API_KEY'] = userdata.get('GOOGLE_API_KEY')
-api_key = os.getenv('GOOGLE_API_KEY')
+#api_key = os.getenv('GOOGLE_API_KEY')
 #if not api_key:
 #	raise ValueError('GOOGLE_API_KEY is not set')
 
-llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp', api_key=SecretStr(api_key))
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp')
 
+async def main():
+    agent = Agent(
+        task="Go to amazon.in, search for Havells 1200 Watts Foldable Hair Dryer and buy it with cash on delivery option, delivery address is:46, 5th Cross, Chennakesava Layout, Valepura Village, Varthur, BENGALURU, KARNATAKA 560087, India, Phone number: 9600437108.",
+        llm=llm,
+        max_actions_per_step=4,
+    )
+    result = await agent.run()
+    print(result)
 
-async def run_search():
-	agent = Agent(
-		task=(
-			'Go to amazon.com, search for laptop, sort by best rating, and give me the price of the first result'
-		),
-		llm=llm,
-		max_actions_per_step=4,
-	)
+asyncio.run(main())
 
-	await agent.run(max_steps=25)
+# async def run_search():
+# 	agent = Agent(
+# 		task=(
+# 			'Go to amazon.com, search for laptop, sort by best rating, and give me the price of the first result'
+# 		),
+# 		llm=llm,
+# 		max_actions_per_step=4,
+# 	)
+
+# 	#await agent.run(max_steps=25)
+# 	#await run_search()
+# 	result = await agent.run()
+# 	print(result)
