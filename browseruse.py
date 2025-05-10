618dea77acc45df6a569dc4db068bd3b5d2fe895
Saravanan Narayanan <saravatpt@gmail.com>
2025-05-10 13:28:31 +0530

    fix: add API key handling and update task for weather information retrieval

--- browseruse.py
+++ browseruse.py
@@ -7,19 +7,20 @@ from pydantic import SecretStr
 from browser_use import Agent
 
 load_dotenv()
-#api_key = os.getenv('GOOGLE_API_KEY')
-#if not api_key:
-#	raise ValueError('GOOGLE_API_KEY is not set')
 
-llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp')
+
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-exp',api_key=os.getenv('GEMINI_API_KEY'))
 
 async def main():
     agent = Agent(
-        task="Go to amazon.in, search for Havells 1200 Watts Foldable Hair Dryer and buy it with cash on delivery option, delivery address is:46, 5th Cross, Chennakesava Layout, Valepura Village, Varthur, BENGALURU, KARNATAKA 560087, India, Phone number: 9600437108.",
+        #task="Go to amazon.in, search for Havells 1200 Watts Foldable Hair Dryer and buy it with cash on delivery option, delivery address is:46, 5th Cross, Chennakesava Layout, Valepura Village, Varthur, BENGALURU, KARNATAKA 560087, India, Phone number: 9600437108.",
+        task="goto google.com and search for bangalore weather and get the weather information",
         llm=llm,
         max_actions_per_step=4,
     )
     result = await agent.run()
+    result.save_as_playwright_script('playwright/script')
+    
     print(result)
 
 asyncio.run(main())
